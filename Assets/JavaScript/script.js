e6c5eb4214b6ef10c9b3ddd26e948daefda81413
Spikey1289 <110435462+Spikey1289@users.noreply.github.com>
2023-12-11 18:42:38 -0500

    finished initial JS Question randomization

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -11,16 +11,16 @@ function shuffle(array){
 }
 
 var questions = {
-    0: { N: 1, Q: "one", A: ["w", "c", "w", "w"] },
-    1: { N: 2, Q: "two", A: ["w", "c", "w", "w"] },
-    2: { N: 3, Q: "three", A: ["w", "c", "w", "w"] },
-    3: { N: 4, Q: "four", A: ["w", "c", "w", "w"] },
-    4: { N: 5, Q: "five", A: ["w", "c", "w", "w"] },
-    5: { N: 6, Q: "six", A: ["w", "c", "w", "w"] },
-    6: { N: 7, Q: "seven", A: ["w", "c", "w", "w"] },
-    7: { N: 8, Q: "eight", A: ["w", "c", "w", "w"] },
-    8: { N: 9, Q: "nine", A: ["w", "c", "w", "w"] },
-    9: { N: 10, Q: "ten", A: ["w", "c", "w", "w"] }
+    0: { N: 1, Q: "one", A: ["C1", "w", "w", "w"] },
+    1: { N: 2, Q: "two", A: ["C2", "w", "w", "w"] },
+    2: { N: 3, Q: "three", A: ["C3", "w", "w", "w"] },
+    3: { N: 4, Q: "four", A: ["C4", "w", "w", "w"] },
+    4: { N: 5, Q: "five", A: ["C5", "w", "w", "w"] },
+    5: { N: 6, Q: "six", A: ["C6", "w", "w", "w"] },
+    6: { N: 7, Q: "seven", A: ["C7", "w", "w", "w"] },
+    7: { N: 8, Q: "eight", A: ["C8", "w", "w", "w"] },
+    8: { N: 9, Q: "nine", A: ["C9", "w", "w", "w"] },
+    9: { N: 10, Q: "ten", A: ["C10", "w", "w", "w"] }
 }
 
 var questList = [];
@@ -33,15 +33,14 @@ console.log(questList);
 questList = shuffle(questList);
 
 for (var i = 0; i < questList.length; i++) {
-    var objectIndex = questList[i]-1;
-    questList[i] = questions[objectIndex].Q;
-    for (var i = 0; i < questions[objectIndex].A.length(); i++){
-        
+    var objectIndex = questList[i] - 1;
+    console.log(questions[objectIndex].Q);
+    answerList = questions[objectIndex].A;
+    answerList = shuffle(answerList);
+    for (var x = 0; x < answerList.length; x++) {
+        console.log(answerList[x]);
     }
-    
 }
-
-console.log(questList);
 
 var storeScores = [];
 
@@ -54,11 +53,22 @@ function highScores(){
 }
 
 function quiz(){
-    var questList = [];
     for (var i = 0; i < 10; i++) {
         questList.push(questions[i].N);
     }
 
+    console.log(questList);
+    questList = shuffle(questList);
+
+    for (var i = 0; i < questList.length; i++) {
+        var objectIndex = questList[i] - 1;
+        console.log(questions[objectIndex].Q);
+        answerList = questions[objectIndex].A;
+        shuffle(answerList);
+        answerList.forEach((index) => {
+            console.log(answerList[index]);
+        });
+    }
 }
 
 function endScreen(){
