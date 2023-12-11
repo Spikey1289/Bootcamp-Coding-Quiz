f98baa295126a06685b9a312b67c2648ea0ec54f
Spikey1289 <110435462+Spikey1289@users.noreply.github.com>
2023-12-11 17:52:37 -0500

    Adding JS Logic for creating a shuffled question array

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,27 +1,49 @@
-var questions = {
-    0: "",
-    1: "",
-    2: "",
-    3: "",
-    4: "",
-    5: "",
-    6: "",
-    7: "",
-    8: "",
-    9: "",
-    10: "",
-    11: "",
-    12: "",
-    13: "",
-    14: "",
-    15: "",
-    16: "",
-    17: "",
-    18: "",
-    19: ""
+function shuffle(array){
+    let index = array.length, randomIndex;
+
+    while (index > 0){
+        randomIndex = Math.floor(Math.random() * index);
+        index--;
+
+        [array[index], array[randomIndex]] = [array[randomIndex], array[index]];
+    }
+    return array;
 }
 
-var scores = [];
+var questions = {
+    0: { N: 1, Q: "one", A: ["w", "c", "w", "w"] },
+    1: { N: 2, Q: "two", A: ["w", "c", "w", "w"] },
+    2: { N: 3, Q: "three", A: ["w", "c", "w", "w"] },
+    3: { N: 4, Q: "four", A: ["w", "c", "w", "w"] },
+    4: { N: 5, Q: "five", A: ["w", "c", "w", "w"] },
+    5: { N: 6, Q: "six", A: ["w", "c", "w", "w"] },
+    6: { N: 7, Q: "seven", A: ["w", "c", "w", "w"] },
+    7: { N: 8, Q: "eight", A: ["w", "c", "w", "w"] },
+    8: { N: 9, Q: "nine", A: ["w", "c", "w", "w"] },
+    9: { N: 10, Q: "ten", A: ["w", "c", "w", "w"] }
+}
+
+var questList = [];
+var answerList = [];
+for (var i = 0; i < 10; i++) {
+    questList.push(questions[i].N);
+}
+
+console.log(questList);
+questList = shuffle(questList);
+
+for (var i = 0; i < questList.length; i++) {
+    var objectIndex = questList[i]-1;
+    questList[i] = questions[objectIndex].Q;
+    for (var i = 0; i < questions[objectIndex].A.length(); i++){
+        
+    }
+    
+}
+
+console.log(questList);
+
+var storeScores = [];
 
 function initialState(){
 
@@ -31,6 +53,18 @@ function highScores(){
 
 }
 
-function Quiz(){
-    
+function quiz(){
+    var questList = [];
+    for (var i = 0; i < 10; i++) {
+        questList.push(questions[i].N);
+    }
+
+}
+
+function endScreen(){
+
+}
+
+function clearScores(){
+    storeScores = [];
 }
