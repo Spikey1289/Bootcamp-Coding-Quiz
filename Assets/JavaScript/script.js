080934eb975685fcad52afd71f4983a47361f940
Spikey1289 <110435462+Spikey1289@users.noreply.github.com>
2024-01-16 15:19:52 -0500

    quiz loads, checks answers, and gives a message.

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,24 +1,29 @@
 var timeEl = document.querySelector(".timer");
 var cardEl = document.querySelector(".card");
-var startBtnEl = document.querySelector("#startbtn");
+var startBtnEl = document.getElementById("startBtn");
+var cardBtnEl = document.querySelector(".cardBtn");
+
 var secondsLeft;
+var questNumber = 0;
 var questList = [];
 var answerList = [];
 var storeScores = [];
 
+//main storage object for the quiz questions
 var questions = {
-    0: { N: 1, Q: "one", A: ["C1", "w", "w", "w"] },
-    1: { N: 2, Q: "two", A: ["C2", "w", "w", "w"] },
-    2: { N: 3, Q: "three", A: ["C3", "w", "w", "w"] },
-    3: { N: 4, Q: "four", A: ["C4", "w", "w", "w"] },
-    4: { N: 5, Q: "five", A: ["C5", "w", "w", "w"] },
-    5: { N: 6, Q: "six", A: ["C6", "w", "w", "w"] },
-    6: { N: 7, Q: "seven", A: ["C7", "w", "w", "w"] },
-    7: { N: 8, Q: "eight", A: ["C8", "w", "w", "w"] },
-    8: { N: 9, Q: "nine", A: ["C9", "w", "w", "w"] },
-    9: { N: 10, Q: "ten", A: ["C10", "w", "w", "w"] }
+    0: { N: 1, Q: "one", A: [["C1", "Correct"], ["w", "Wrong"], ["w", "Wrong"], ["w", "Wrong"]] },
+    1: { N: 2, Q: "two", A: [["C2", "Correct"], ["w", "Wrong"], ["w", "Wrong"], ["w", "Wrong"]] },
+    2: { N: 3, Q: "three", A: [["C3", "Correct"], ["w", "Wrong"], ["w", "Wrong"], ["w", "Wrong"]] },
+    3: { N: 4, Q: "four", A: [["C4", "Correct"], ["w", "Wrong"], ["w", "Wrong"], ["w", "Wrong"]] },
+    4: { N: 5, Q: "five", A: [["C5", "Correct"], ["w", "Wrong"], ["w", "Wrong"], ["w", "Wrong"]] },
+    5: { N: 6, Q: "six", A: [["C6", "Correct"], ["w", "Wrong"], ["w", "Wrong"], ["w", "Wrong"]] },
+    6: { N: 7, Q: "seven", A: [["C7", "Correct"], ["w", "Wrong"], ["w", "Wrong"], ["w", "Wrong"]] },
+    7: { N: 8, Q: "eight", A: [["C8", "Correct"], ["w", "Wrong"], ["w", "Wrong"], ["w", "Wrong"]] },
+    8: { N: 9, Q: "nine", A: [["C9", "Correct"], ["w", "Wrong"], ["w", "Wrong"], ["w", "Wrong"]] },
+    9: { N: 10, Q: "ten", A: [["C10", "Correct"], ["w", "Wrong"], ["w", "Wrong"], ["w", "Wrong"]] }
 }
 
+//when called, shuffles the input array and returns it
 function shuffle(array){
     let index = array.length, randomIndex;
 
@@ -31,28 +36,91 @@ function shuffle(array){
     return array;
 }
 
+//sets the page to its initial state when called
 function initialState() {
+    timeEl.textContent = "60";
+}
+
+//re-populates the questList array
+function populateQuestions() {
+    questList = [];
+
+    for (let i = 0; i < 10; i++) {
+        questList.push(questions[i].N);
+    }
+    questList = shuffle(questList);
+
 
 }
 
-function quiz() {
+//starts the quiz when start button is pressed
+function startQuiz() {
+
+    //resets question number counter
+    questNumber = 0;
+
+    //start timer
     Timer();
 
-    for (var i = 0; i < 10; i++) {
-        questList.push(questions[i].N);
+    //removes start button
+    startBtnEl.style.display = 'none';
+
+    //randomizes the questions
+    populateQuestions();
+
+    //generates the first question
+    questionGen();
+}
+
+//generates a question based off of questNumber
+function questionGen() {
+    //the questList is randomized, but it's 1-10, so you need to subtract 1 to get the index to match
+    let objectIndex = questList[questNumber] - 1;
+
+    if(questNumber >= 10){
+        // endScreen();
+        console.log("done")
+        return;
     }
 
-    console.log(questList);
-    questList = shuffle(questList);
+    //sets the H1 to the question
+    document.querySelector('.cardH1').innerText = questions[objectIndex].Q;
 
-    for (var i = 0; i < questList.length; i++) {
-        var objectIndex = questList[i] - 1;
-        console.log(questions[objectIndex].Q);
-        answerList = questions[objectIndex].A;
-        answerList = shuffle(answerList);
-        for (var x = 0; x < answerList.length; x++) {
-            console.log(answerList[x]);
+    //sets the answers list
+    answerList = questions[objectIndex].A;
+    answerList = shuffle(answerList);
+    // console.log(answerList);
+    let buttonsEL = document.querySelectorAll("#questions li");
+
+    //makes answer buttons based off of the answer list
+    for (let i = 0; i < answerList.length; i++) {
+        let letter = "";
+        switch(true){
+            case (i === 0):
+                letter = "A - ";
+            break;
+            case (i === 1):
+                letter = "B - ";
+                break;
+            case (i === 2):
+                letter = "C - ";
+                break;
+            case (i === 3):
+                letter = "D - ";
+                break;
         }
+        buttonsEL[i].innerHTML = letter + "<button id='"+ answerList[i][0] +"'>" + answerList[i][1] +"</button>";
+    }
+}
+
+//checks if the ID fed to it is 'w'. If it is not, next question
+function checkAnswer(i){
+    if (i === "w"){
+        secondsLeft--;
+        document.querySelector("#feedback").textContent = "wrong, -1 second";
+    } else {
+        document.querySelector("#feedback").textContent = "correct!";
+        questNumber++;
     }
 }
 
@@ -60,19 +128,21 @@ function highScores() {
 
 }
 
+//displays the end screed, resets everything
 function endScreen() {
-
+    
 }
 
 function Timer() {
     secondsLeft = 60;
     var time = setInterval(function () {
         secondsLeft--;
-        timeEl.textContent = secondsLeft + "";
+        timeEl.textContent = secondsLeft;
 
-        if (secondsLeft === 0) {
+        if (secondsLeft <= 0) {
             // endScreen();
             clearInterval(time);
+            console.log("done");
             return;
         }
     }, 1000);
@@ -81,3 +151,16 @@ function Timer() {
 function clearScores() {
     storeScores = [];
 }
+
+initialState();
+
+document.querySelector('#startBtn').addEventListener("click", function(){
+    startQuiz();
+})
+
+document.querySelector("#questions").addEventListener("click", function(evt){
+    var target = evt.target.id;
+    // console.log(target);
+    checkAnswer(target);
+    questionGen();
+})
